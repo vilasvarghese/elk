d5f1a75aed2ed693cf2dd7d08485224b65929285
vilasvarghese <vilas.varghese@gmail.com>
2023-10-13 21:19:00 +0530

    initial commit

---
+++ src/test/java/com/elk/demo/ElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
